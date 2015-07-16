87af6ab14b7e9c22ee476437e20d5c61aca908ef
Max Shaffer <mshaffer@digitallumens.com>
2015-07-16 18:38:58 -0400

    basic autocomplete is working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,21 +2,82 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 
+QString keywordsReadOnly = "firmwareVersion,temperature";
+QString keywordsReadWrite = "productCode,serialNumber,unixTime,lightManualLevel";
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  uiInit();
+  completerInit();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
+}
+
+void MainWindow::uiInit()
+{
+  ui->actionDisconnect->setVisible(false);
+}
+
+void MainWindow::completerInit()
+{
+  QStringList list;
+  QStringList::const_iterator constIterator;
+  // build a dictionary of keywords
+  list = keywordsReadOnly.split(",");
+  for (constIterator = list.constBegin(); constIterator != list.constEnd(); ++constIterator) {
+    keywordList << (QString)"get " + (QString)(*constIterator).toLocal8Bit().constData();
+  }
+  list = keywordsReadWrite.split(",");
+  for (constIterator = list.constBegin(); constIterator != list.constEnd(); ++constIterator) {
+    keywordList << (QString)"get " + (QString)(*constIterator).toLocal8Bit().constData();
+    keywordList << (QString)"set " + (QString)(*constIterator).toLocal8Bit().constData();
+  }
+  completer = new QCompleter(keywordList, this);
+  completer->setCaseSensitivity(Qt::CaseInsensitive);
+  ui->lineEdit->setCompleter(completer);
 }
 
 void MainWindow::on_lineEdit_returnPressed()
 {
-    ui->plainTextEdit->appendPlainText(ui->lineEdit->text());
-    ui->lineEdit->clear();
-    qDebug() << "got it";
+  ui->plainTextEdit->appendPlainText(ui->lineEdit->text());
+  ui->lineEdit->clear();
+}
+
+void MainWindow::on_actionUseFTDICable_triggered()
+{
+  ui->actionUseTelegesisAdapter->setChecked(false);
+}
+
+void MainWindow::on_actionUseTelegesisAdapter_triggered()
+{
+  ui->actionUseFTDICable->setChecked(false);
+}
+
+void MainWindow::on_actionConnect_triggered()
+{
+  ui->actionConnect->setVisible(false);
+  ui->actionDisconnect->setVisible(true);
+  ui->actionUseFTDICable->setDisabled(true);
+  ui->actionUseTelegesisAdapter->setDisabled(true);
+  ui->actionConfigure->setDisabled(true);
+}
+
+void MainWindow::on_actionDisconnect_triggered()
+{
+  ui->actionDisconnect->setVisible(false);
+  ui->actionConnect->setVisible(true);
+  ui->actionUseFTDICable->setDisabled(false);
+  ui->actionUseTelegesisAdapter->setDisabled(false);
+  ui->actionConfigure->setDisabled(false);
+}
+
+void MainWindow::on_actionConfigure_triggered()
+{
+  qDebug() << "configure all the things!";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCompleter>
 
 namespace Ui {
 class MainWindow;
@@ -17,9 +18,18 @@ public:
 
 public slots:
     void on_lineEdit_returnPressed();
+    void on_actionUseFTDICable_triggered();
+    void on_actionUseTelegesisAdapter_triggered();
+    void on_actionConnect_triggered();
+    void on_actionDisconnect_triggered();
+    void on_actionConfigure_triggered();
 
 private:
     Ui::MainWindow *ui;
+    void uiInit();
+    void completerInit();
+    QStringList keywordList;
+    QCompleter *completer;
 };
 
 #endif // MAINWINDOW_H
